d402102620a8d87ebe200837cc046308f3dfdade
Evandro Zanatta <hemanstifler@gmail.com>
2020-04-22 18:20:25 -0300

    Corrigindo exceção de rota

--- assets/js/config/verify_token.js
+++ assets/js/config/verify_token.js
@@ -7,4 +7,9 @@ if(location.pathname != '/configuracoes/'){
         window.trackview_slug = localStorage.getItem('trackview_slug');
     }
 
+}else{
+    if((localStorage.getItem('trackview_token') != null) && (localStorage.getItem('trackview_slug') != null)){
+        window.trackview_token = localStorage.getItem('trackview_token');
+        window.trackview_slug = localStorage.getItem('trackview_slug');
+    }
 }
